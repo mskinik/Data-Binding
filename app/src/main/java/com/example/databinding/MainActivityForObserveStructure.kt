2355d47f76d98b66917e446ce6163f6d4814f4ff
Mustafa Süleyman Kınık <suleyman.kinik@voltlines.com>
2022-01-22 23:51:49 +0300

    Created new main launcher act for showing observable structure together data-binding on code base

---
+++ app/src/main/java/com/example/databinding/MainActivityForObserveStructure.kt
@@ -0,0 +1,11 @@
+package com.example.databinding
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityForObserveStructure : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_for_observe_structure)
+    }
+}
